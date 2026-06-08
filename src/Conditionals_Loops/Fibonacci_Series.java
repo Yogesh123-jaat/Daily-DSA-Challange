c9b7e2ea32a14b3e1a5e531dd42ac4c5d7ce20c6
Yogesh Jaat <yc307328@gmail.com>
2026-06-08 12:55:17 +0530

    Solved Problem of Fibonacci Series

---
+++ src/Conditionals_Loops/Fibonacci_Series.java
@@ -0,0 +1,30 @@
+package Conditionals_Loops;
+
+import java.util.Scanner;
+
+public class Fibonacci_Series {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int a = 0 , b = 1 , c , n;
+		
+		System.out.println("Enter the number of terms : ");
+		n = sc.nextInt();
+		
+		System.out.print(a + " " + b);
+		
+		for(int i = 3; i <= n; i++)
+		{
+			c = a + b;
+			
+			System.out.print(" " + c);
+			
+			a = b;
+			b = c;
+			
+		}
+	}
+
+}
