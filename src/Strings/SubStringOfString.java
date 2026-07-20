858302bcf9e8ad29721389cb56f5725afcf6d6b6
Yogesh Jaat <yc307328@gmail.com>
2026-07-20 09:01:43 +0530

    Sub string revision

---
+++ src/Strings/SubStringOfString.java
@@ -0,0 +1,19 @@
+package Strings;
+
+public class SubStringOfString {
+
+	public static void main(String[] args) 
+	{
+		String s = "gopi";
+		
+		for(int i = 0; i < s.length(); i++)
+		{
+			for(int j = i; j < s.length(); j++)
+			{
+				System.out.print(s.substring(i,j+1) + " ");
+			}
+			System.out.println();
+		}
+	}
+
+}
