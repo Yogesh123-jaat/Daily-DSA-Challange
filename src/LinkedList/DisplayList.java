848639872cee691e530128808edaa9fc91be56c2
Yogesh Jaat <yc307328@gmail.com>
2026-07-31 13:11:45 +0530

    Create Own LinkedList DataStructure by RaghavGarg Sir

---
+++ src/LinkedList/DisplayList.java
@@ -0,0 +1,59 @@
+package LinkedList;
+
+public class DisplayList 
+{
+	public static int get(Node head , int idx)
+	{
+		Node temp = head;
+		
+		for(int i = 0; i < idx; i++)
+		{
+			temp = temp.next;
+		}
+		
+		return temp.val;
+	}
+	public static void displayRec(Node head)
+	{
+		if(head == null) return;
+		System.out.print(head.val + " ");
+		displayRec(head.next);
+	}
+	public static void display(Node head)
+	{
+//		Node temp = head;
+//		
+//		while(temp != null)
+//		{
+//			System.out.print(temp.val + " ");
+//			temp = temp.next;
+//		}
+//		
+//		System.out.println();
+		
+		for(Node temp = head; temp != null; temp = temp.next)
+		{
+			System.out.print(temp.val + " ");
+		}
+	}
+	public static void main(String[] args) 
+	{
+		Node a = new Node(10);
+		Node b = new Node(20);
+		Node c = new Node(30);
+		Node d = new Node(40);
+		Node e = new Node(50);
+		
+		a.next = b;
+		b.next = c;
+		c.next = d;
+		d.next = e;
+		e.next = null;  // optional
+		
+		display(a);
+//		displayRec(a);
+		
+		System.out.println();
+		System.out.println(get(a , 2));
+	}
+}
