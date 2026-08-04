197e6899f8843bc2bbb839eec9319f4d1d989a41
Yogesh Jaat <yc307328@gmail.com>
2026-08-04 07:43:39 +0530

    Reverse A LinkedList

---
+++ src/LinkedList/ReverseALinkedList206L.java
@@ -0,0 +1,51 @@
+package LinkedList;
+
+import java.util.ArrayList;
+
+public class ReverseALinkedList206L 
+{
+	public static void main(String[] args) 
+	{
+		
+	}
+	
+	public static Node reverse(Node head)
+	{
+		Node curr = head;
+        Node prev = null;
+        Node fwd = null;
+        
+        while(curr != null)
+        {
+            fwd = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = fwd;
+        }
+        
+        return prev;
+	}
+	
+	public static Node reverseList(Node head) 
+	{        
+        if(head == null) return null;
+        Node temp = head;
+        ArrayList<Node> arr = new ArrayList<>();
+        
+        while(temp != null)
+        {
+            arr.add(temp);
+            temp = temp.next;
+        }
+        
+        int n = arr.size();
+        for(int i = n - 1; i >= 1; i--)
+        {
+            arr.get(i).next = arr.get(i-1);
+        }
+        arr.get(0).next = null;
+        return arr.get(n-1);
+    }
+	
+	
+}
