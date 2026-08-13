355ebcd49572db689cea9ad83e0546333ba968a8
Yogesh123-jaat <yc307328@gmail.com>
2026-08-13 09:07:46 +0530

    reverse doubly ll GFG practice

---
+++ src/LinkedList/Doubly/ReverseDoublyLinkedList.java
@@ -0,0 +1,41 @@
+package LinkedList.Doubly;
+
+public class ReverseDoublyLinkedList 
+{
+	public static void main(String[] args) 
+	{
+		
+	}
+	
+	public static ListNode reverseDLL(ListNode head)
+	{
+		ListNode curr = head;
+		ListNode pre = null;
+		ListNode fwd = null;
+		
+		while(curr != null)
+		{
+			fwd = curr.next;
+			curr.next = pre;
+			curr.prev = fwd;
+			pre = curr;
+			curr = fwd;
+		}
+		return pre;
+	}
+	
+	public static ListNode reverse(ListNode head)
+	{
+		ListNode curr = head;
+		ListNode temp = null;
+		
+		while(curr != null)
+		{
+			temp = curr.prev;
+			curr.prev = curr.next;
+			curr.next = temp;
+			curr = curr.prev;
+		}
+		return temp.prev;
+	}
+}
