6d9c0a8e6b745ae769955db2138224ce4b7bc5af
Yogesh123-jaat <yc307328@gmail.com>
2026-08-18 22:31:50 +0530

    another approach

--- src/Stack/SpanStockProblemGFG.java
+++ src/Stack/SpanStockProblemGFG.java
@@ -3,12 +3,50 @@ package Stack;
 import java.util.ArrayList;
 import java.util.Stack;
 
+class Pair{
+    int val;
+    int idx;
+    
+    Pair(int val , int idx)
+    {
+        this.val = val;
+        this.idx = idx;
+    }
+}
+
 public class SpanStockProblemGFG 
 {
 	public static void main(String[] args) 
 	{
 		
 	}
+    
+    public ArrayList<Integer> calculateSpan2(int[] arr) 
+    {
+        int n = arr.length;
+        int[] span = new int[n];
+        span[0] = 1;
+        Stack<Pair> st = new Stack<>();
+        st.push(new Pair(arr[0] , 0));
+        
+        for(int i = 1; i < n; i++)
+        {
+            while(st.size() > 0 && arr[i] >= st.peek().val) st.pop();
+            if(st.size() == 0) span[i] = i - (-1);
+            else span[i] = i - st.peek().idx;
+            
+            st.push(new Pair(arr[i] , i));
+        }
+        
+        ArrayList<Integer> ans = new ArrayList<>();
+        
+        for(int i = 0; i < n; i++)
+        {
+            ans.add(span[i]);
+        }
+        
+        return ans;
+    }
 	
 	public ArrayList<Integer> calculateSpan(int[] arr) 
     {
