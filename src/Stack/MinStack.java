d9622eafbe44224a30e2f1ce3bf242f85c136907
Yogesh123-jaat <yc307328@gmail.com>
2026-08-22 22:41:13 +0530

    Another method

--- src/Stack/MinStack.java
+++ src/Stack/MinStack.java
@@ -41,3 +41,46 @@ public class MinStack
         return MinStack.peek();
     }
 }
+
+class AnotherMethod {
+    Stack<Long> MainStack;
+    long min;
+    public AnotherMethod() 
+    {   
+        MainStack = new Stack();
+        min = Long.MAX_VALUE;
+    }
+    
+    public void push(int value) 
+    {
+        long val = (long)value;
+        if(MainStack.size() == 0) min = val;
+        if(val >= min)
+        {
+            MainStack.push(val);
+        } 
+        else
+        {
+            MainStack.push(val + val - min);
+            min =val;
+        }
+    }
+    
+    public void pop() 
+    {
+        if(MainStack.peek() < min) min = min + (min - MainStack.peek());
+        MainStack.pop();
+    }
+    
+    public int top() 
+    {
+        long a = MainStack.peek();
+        if(a < min) return (int)min;
+        else return (int)a;
+    }
+    
+    public int getMin() 
+    {
+        return (int)min;
+    }
+}
