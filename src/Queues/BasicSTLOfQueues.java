f282a9b7ab8b7f5560730fafe72c1f085ecaaae2
Yogesh123-jaat <yc307328@gmail.com>
2026-08-23 10:23:19 +0530

    collection frameworks

---
+++ src/Queues/BasicSTLOfQueues.java
@@ -0,0 +1,23 @@
+package Queues;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BasicSTLOfQueues 
+{
+	public static void main(String[] args)
+	{
+		Queue<Integer> q = new LinkedList<>();
+		
+		q.add(10);
+		q.add(20);
+		q.add(30);
+		q.add(40);
+		
+		System.out.println(q + " " + q.peek());
+		
+		q.remove();
+		System.out.println(q + " " + q.size());
+		System.out.println(q.poll() + " " + q);
+	}
+}
