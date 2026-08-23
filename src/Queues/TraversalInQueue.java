186d06ee789bbda3fc5bbe5ecced4bd759e4f343
Yogesh123-jaat <yc307328@gmail.com>
2026-08-23 10:45:03 +0530

    Traversal In Queue

---
+++ src/Queues/TraversalInQueue.java
@@ -0,0 +1,25 @@
+package Queues;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class TraversalInQueue 
+{
+	public static void main(String[] args) 
+	{
+		Queue<Integer> q = new LinkedList<>();
+		
+		q.add(10);
+		q.add(20);
+		q.add(30);
+		q.add(40);
+		q.add(50);
+		
+		int n = q.size();
+		for(int i = 1; i <= n; i++)
+		{
+			System.out.print(q.peek() + " ");
+			q.add(q.remove());
+		}
+	}
+}
