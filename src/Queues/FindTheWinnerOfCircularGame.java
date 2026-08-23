c93b3e9bde9847dd1e30945412afbce19092bb3e
Yogesh123-jaat <yc307328@gmail.com>
2026-08-23 11:49:28 +0530

    Find The Winner Of Circular Game leetcode 1823

---
+++ src/Queues/FindTheWinnerOfCircularGame.java
@@ -0,0 +1,32 @@
+package Queues;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class FindTheWinnerOfCircularGame
+{
+	public static void main(String[] args) 
+	{
+		
+	}
+	
+	public static int findTheWinner(int n, int k) 
+    {
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 1; i <= n; i++)
+        {
+            q.add(i);
+        }    
+
+        while(q.size() > 1)
+        {
+            for(int i = 1; i <= k-1; i++)
+            {
+                q.add(q.remove());
+            }
+            q.remove();
+        }
+
+        return q.peek();
+    }
+}
