6bc39ca821eab996a1ea6079a2251314664f781e
Yogesh123-jaat <yc307328@gmail.com>
2026-08-24 09:12:15 +0530

    Implement stack using queue push efficient

---
+++ src/Queues/ImplementStackUsingQueue.java
@@ -0,0 +1,53 @@
+package Queues;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class MyStack               // Push Efficient Approacgh
+{
+    Queue<Integer> q = new LinkedList<>();
+    public MyStack() {
+        
+    }
+    
+    public void push(int x) 
+    {
+        q.add(x);    
+    }
+    
+    public int pop() 
+    {
+        int n = q.size();
+        for(int i = 1; i <= n - 1; i++)
+        {
+            q.add(q.remove());
+        }    
+        return q.remove();
+    }
+    
+    public int top() 
+    {
+        int n = q.size();
+        for(int i = 1; i <= n - 1; i++)
+        {
+            q.add(q.remove());
+        }
+
+        int p = q.peek();
+        q.add(q.remove());
+
+        return p;
+    }
+    
+    public boolean empty() 
+    {
+        return (q.size() == 0);    
+    }
+}
+public class ImplementStackUsingQueue 
+{
+	public static void main(String[] args) 
+	{
+		
+	}
+}
