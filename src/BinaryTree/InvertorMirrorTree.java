61fa7f27c490ca2c807f9be56eb16b03b3f813ef
Yogesh123-jaat <yc307328@gmail.com>
2026-08-26 09:01:25 +0530

    Some Another methods of invert tree

--- src/BinaryTree/InvertorMirrorTree.java
+++ src/BinaryTree/InvertorMirrorTree.java
@@ -6,15 +6,39 @@ public class InvertorMirrorTree
 	{
 		
 	}
-	public static Node invertTree(Node root) 
+	public static Node invertTreePre(Node root)    // preorder postorder kaam kar jaayega
     {
         if(root == null) return null;
         Node temp = root.left;
         root.left = root.right;
         root.right = temp;
 
-        invertTree(root.left);
-        invertTree(root.right);
+        invertTreePre(root.left);
+        invertTreePre(root.right);
+
+        return root;
+    }
+	public static Node invertTreeIn(Node root)    // preorder postorder kaam kar jaayega
+    {
+        if(root == null) return null;
+
+        invertTreeIn(root.left);
+        Node temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertTreeIn(root.left);
+
+        return root;
+    }
+	public static Node invertTreePost(Node root)    // preorder postorder kaam kar jaayega
+    {
+        if(root == null) return null;
+
+        invertTreePost(root.left);
+        invertTreePost(root.right);
+        Node temp = root.left;
+        root.left = root.right;
+        root.right = temp;
 
         return root;
     }
