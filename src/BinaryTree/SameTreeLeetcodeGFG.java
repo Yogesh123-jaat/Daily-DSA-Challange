7241379a47f552640b39c788f3e632ee667bf794
Yogesh123-jaat <yc307328@gmail.com>
2026-08-31 10:43:46 +0530

    same tree on leetcode and GFG both

---
+++ src/BinaryTree/SameTreeLeetcodeGFG.java
@@ -0,0 +1,16 @@
+package BinaryTree;
+
+public class SameTreeLeetcodeGFG 
+{
+	public static void main(String[] args) 
+	{
+		
+	}
+	public static boolean isIdentical(Node p , Node q)
+	{
+		if(p == null && q == null) return true;
+		if(p == null || q == null) return false;
+		if(p.val != q.val) return false;
+		return isIdentical(p.left , q.left) && isIdentical(q.left , q.right);
+	}
+}
