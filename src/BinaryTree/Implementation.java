e01b6e9383e8afcd75bf23bf93ec74c6b064c65a
Yogesh123-jaat <yc307328@gmail.com>
2026-09-01 08:01:02 +0530

    Level wise order printing BFS

--- src/BinaryTree/Implementation.java
+++ src/BinaryTree/Implementation.java
@@ -1,5 +1,8 @@
 package BinaryTree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 class Node
 {
 	int val;
@@ -11,6 +14,19 @@ class Node
 		this.val = val;
 	}
 }
+
+class Pair
+{
+	Node node;
+	int level;
+	
+	Pair(Node node , int level)
+	{
+		this.node = node;
+		this.level = level;
+	}
+}
+
 public class Implementation 
 {
 	public static void main(String[] args) 
@@ -33,17 +49,56 @@ public class Implementation
 		b.left = d; b.right = e;
 		c.left = f; c.right = g;
 		
-		preOrder(a);
-		inOrder(a);
-		postOrder(a);
-		System.out.println();
-		System.out.println(size(a));
-		System.out.println(sum(a));
-		System.out.println(product(a));
-		System.out.println(productNonZero(a));
-		System.out.println(max(a));
-		System.out.println(min(a));
-		System.out.println(levels(a));
+//		preOrder(a);
+//		inOrder(a);
+//		postOrder(a);
+//		System.out.println();
+//		System.out.println(size(a));
+//		System.out.println(sum(a));
+//		System.out.println(product(a));
+//		System.out.println(productNonZero(a));
+//		System.out.println(max(a));
+//		System.out.println(min(a));
+//		System.out.println(levels(a));
+//		levelOrder(a);
+		
+		levelOrderLineWise(a);
+	}
+	
+	public static void levelOrder(Node root)
+	{
+		Queue<Node> q = new LinkedList<>();
+		q.add(root);
+		
+		while(q.size() > 0)
+		{
+			Node front = q.remove();
+			System.out.print(front.val + " ");
+			if(front.left != null) q.add(front.left);
+			if(front.right != null) q.add(front.right);
+		}
+		
+	}
+	
+	public static void levelOrderLineWise(Node root)
+	{
+		Queue<Pair> q = new LinkedList<>();
+		int currLevel = 0;
+		q.add(new Pair(root , 0));
+		
+		while(q.size() > 0)
+		{
+			Pair front = q.remove();
+			if(front.level != currLevel)
+			{
+				currLevel++;
+				System.out.println();
+			}
+			
+			System.out.print(front.node.val + " ");
+			if(front.node.left != null) q.add(new Pair(front.node.left , front.level + 1));
+			if(front.node.right != null) q.add(new Pair(front.node.right , front.level + 1));
+		}
 	}
 	
 	public static int levels(Node root)
